4097cc54a7caaaf152c30155a532b04f4885978f
santiagoTorres27 <santiago.torres27@outlook.com>
2022-11-27 02:50:43 +0100

    Ejercicios sesion 1

---
+++ Sesion1/Sesion1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Escribe tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre + "!");
+
+string hora = DateTime.Now.ToString();
+Console.WriteLine("Son las " + hora);
