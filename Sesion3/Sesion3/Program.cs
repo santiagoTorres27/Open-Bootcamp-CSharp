ed5ccd54848a8da08f6032437b7f61f0730c2b00
santiagoTorres27 <santiago.torres27@outlook.com>
2022-11-28 01:16:15 +0100

    Ejercicios sesion 3

---
+++ Sesion3/Sesion3/Program.cs
@@ -0,0 +1,27 @@
+﻿Client c1 = new Client("Santiago", "Torres", "987654", "Madrid", "santiago@gmail.com", false);
+Console.WriteLine(c1);
+
+public struct Client
+{
+    public string name;
+    public string lastname;
+    public string phone;
+    public string address;
+    public string email;
+    public bool isNewClient;
+
+    public Client(string name, string lastname, string phone, string address, string email, bool isNewClient)
+    {
+        this.name = name;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.isNewClient = isNewClient;
+    }
+
+    public override string? ToString()
+    {
+        return $"({name}, {lastname}, {phone}, {address}, {email}, New client = {isNewClient})";
+    }
+}
