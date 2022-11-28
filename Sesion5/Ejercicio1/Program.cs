f4606cfca07f6471072dc4ccd5bd6811fe7b8f17
santiagoTorres27 <santiago.torres27@outlook.com>
2022-11-28 18:44:19 +0100

    Ejercicios sesion 5

---
+++ Sesion5/Ejercicio1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Console.WriteLine("Nombre: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Email: ");
+string email = Console.ReadLine();
+
+Console.WriteLine("Tiene descuento? S/N: ");
+string discountStr = Console.ReadLine();
+bool discount = (discountStr.ToLower() == "s") ? true : false;
+
+Client c = new Client();
+c.Name = name;
+c.Email = email;
+c.Coupon = discount;
+
+double price = 24.43;
+Console.WriteLine("Cliente: ");
+Console.WriteLine(c);
+
+double finalPrice = (c.Coupon) ? price - (price * 0.5) : price;
+Console.WriteLine("Precio final: " + finalPrice);
+
+public struct Client
+{
+    private string name;
+    private string email;
+    private bool coupon;
+
+    public Client(string name, string email, bool coupon) : this()
+    {
+        Name = name;
+        Email = email;
+        Coupon = coupon;
+    }
+
+    public string Name { get => name; set => name = value; }
+    public string Email { get => email; set => email = value; }
+    public bool Coupon { get => coupon; set => coupon = value; }
+
+    public override string? ToString()
+    {
+        return $"{name}, {email}, {coupon}";
+    }
+}
---
+++ Sesion5/Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿List<String> programmingLanguages = new List<string> { "Java", "C#", "JavaScript" };
+
+Console.WriteLine("Lenguajes de programación: ");
+for (int i = 0; i < programmingLanguages.Count; i++)
+{
+    Console.WriteLine($"{i + 1}: {programmingLanguages[i]}");
+}
+
+Console.WriteLine("Selecciona un lenguaje: ");
+int option = Convert.ToInt32(Console.ReadLine());
+string languageSelected = "";
+
+switch (option)
+{
+    case 1:
+        languageSelected = programmingLanguages[0];
+        break;
+
+    case 2:
+        languageSelected = programmingLanguages[1];
+        break;
+
+    case 3:
+        languageSelected = programmingLanguages[2];
+        break;
+    default:
+        languageSelected = "Ninguno";
+        break;
+}
+
+Console.WriteLine($"Has seleccionado: {languageSelected}");
